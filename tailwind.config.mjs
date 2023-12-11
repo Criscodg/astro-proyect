c3a5b591465408278128aa7a064bcc4c004590b2
fredyjxms <152222770+fredyjxms@users.noreply.github.com>
2023-12-11 09:07:59 -0500

    Add files via upload
    Incorporación diseño landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				'poppins': ['Poppins', 'sans-serif'],
+				'roboto': ['Roboto', 'sans-serif'],
+				'inter': ['Inter', 'sans-serif']
+			}
+			
+		},
 	},
 	plugins: [],
 }
